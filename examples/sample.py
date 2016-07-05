0440a107e1a719262055de104982ad69ba1fbfc1
Tarun <tarun86@gmail.com>
2016-07-05 14:13:32 +0530

    Before switching to a branch

--- examples/sample.py
+++ examples/sample.py
@@ -15,4 +15,9 @@ def string_operations():
     print 'My name is {0} and I am {1} years old'.format(name, age)
     print 'My name is {name} and I am {age} years old'.format(name=name, age=age)
     
+
+#Strings Are Immutable .This means that once you have created a string, you cannot change it. Although this might seem like a bad thing, it really isn't. We will see why this is not a limitation in the various programs that we see later on
+
+
+
 string_operations()
