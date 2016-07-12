41431e0f0fe6ecff7ad3fd1816723646c3951599
Tarun <tarun86@gmail.com>
2016-07-12 19:52:14 +0530

    11th july

--- examples/classes.py
+++ examples/classes.py
@@ -30,18 +30,38 @@ class customer_segmentation():
         :return:
         '''
 
+    def filter_dataframe(self, cols):
+        self.group_by_obj.columns = cols
+        self.data_frame = self.data_frame[~self.data_frame['car_id'].isin(self.group_by_obj[self.group_by_obj['count']>1]['car_id'].unique())]
+        self.data_frame = self.data_frame[self.data_frame['days']>0]
+        # print self.data_frame
+
     def group_by(self, cols):
-        self.group_by_obj = self.data_frame.groupby(cols)
+        self.group_by_obj = self.data_frame.groupby(cols).size().reset_index()
+        cols.append('count')
+        self.filter_dataframe(cols=cols)
+
+    def derived_features(self):
+        lamdbafunc = lambda x: pd.Series([(x['days'] - x['date_interval']) / x['date_interval'],
+                            (x['mileage'] - x['mileage_interval']) / x['mileage_interval'],
+                            x['mileage'] / x['days']])
+        self.data_frame[['days_normalise', 'mileage_normalise', 'avg_run_per_day']] = self.data_frame.apply(lamdbafunc, axis = 1)
+        # x = self.data_frame.apply(lamdbafunc, axis = 1)
+
+
+        # self.data_frame['days_normalise']= (self.data_frame['days'] - self.data_frame['date_interval']) / self.data_frame['date_interval']
+        # self.data_frame['mileage_normalise']= (self.data_frame['mileage'] - self.data_frame['mileage_interval']) / self.data_frame['mileage_interval']
+        # self.data_frame['avg_run_per_day']= self.data_frame['mileage'] / self.data_frame['days']
 
 
 if __name__ == '__main__':
     try:
         data_analysis = customer_segmentation()
         data_analysis.read_csv('customer.csv')
-        print data_analysis.data_frame.head()
         group_by_cols = ['car_id','service_type']
         data_analysis.group_by(group_by_cols)
-        print data_analysis.group_by_obj.
-
-    except:
-        print  "something went wrong"
+        data_analysis.derived_features()
+        print "hello world"
+        # print data_analysis.group_by_obj
+    except Exception() as err:
+        print  err
